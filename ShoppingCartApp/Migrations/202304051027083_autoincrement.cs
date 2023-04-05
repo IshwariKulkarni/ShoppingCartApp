30547c63e3198d41012da67dd77d5a6f6f4abef5
Ishwari Kulkarni <ishwarikulkarni78@gmail.com>
2023-04-05 15:58:21 +0530

    add2

---
+++ ShoppingCartApp/Migrations/202304051027083_autoincrement.cs
@@ -0,0 +1,26 @@
+﻿namespace ShoppingCartApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class autoincrement : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.OrderDetails", "UserId", "dbo.Customers");
+            DropPrimaryKey("dbo.Customers");
+            AlterColumn("dbo.Customers", "UserId", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Customers", "UserId");
+            AddForeignKey("dbo.OrderDetails", "UserId", "dbo.Customers", "UserId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderDetails", "UserId", "dbo.Customers");
+            DropPrimaryKey("dbo.Customers");
+            AlterColumn("dbo.Customers", "UserId", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Customers", "UserId");
+            AddForeignKey("dbo.OrderDetails", "UserId", "dbo.Customers", "UserId", cascadeDelete: true);
+        }
+    }
+}
